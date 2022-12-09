edb425ad6a51b0b393964f2b619feceb0d7deee2
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2022-12-09 08:22:27 +0530

    ✨ added comments

--- randomJokeGen/script.js
+++ randomJokeGen/script.js
@@ -2,6 +2,17 @@ const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
 const url= "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
+/* 
+Basic premise of this project for me is to improve css and learn to work with api
+
+Here:
+Every time getJoke is invoked, the class fade from present item get removed and url is fetched from api and class fade is added
+to it, hence it gets displayed.
+*/
+
+
+
+
 let getJoke =() =>{
    jokeContainer.classList.remove("fade");
     fetch(url)
