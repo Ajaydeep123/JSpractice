bcd3293c596d2a3bd1d8b8c579bb221f9adf661b
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2022-12-20 06:19:49 +0530

    ✨ promises in async js, fetch

--- Revision/promises_inter.js
+++ Revision/promises_inter.js
@@ -87,20 +87,88 @@ promise.then(script => alert('Another handler...'));
 
 
 
-//thenable
-class Thenable {
-  constructor(num) {
-    this.num = num;
-  }
-  then(resolve, reject) {
-    alert(resolve); // function() { native code }
-    // resolve with this.num*2 after the 1 second
-    setTimeout(() => resolve(this.num * 2), 1000); // (**)
-  }
+// //thenable
+// class Thenable {
+//   constructor(num) {
+//     this.num = num;
+//   }
+//   then(resolve, reject) {
+//     alert(resolve); // function() { native code }
+//     // resolve with this.num*2 after the 1 second
+//     setTimeout(() => resolve(this.num * 2), 1000); // (**)
+//   }
+// }
+
+// new Promise(resolve => resolve(1))
+//   .then(result => {
+//     return new Thenable(result); // (*)
+//   })
+//   .then(alert); // shows 2 after 1000ms
+
+
+
+// Example of promises in network request : fetch ,makes a network request to the url and returns a promise. 
+
+// fetch('/article/promise-chaining/user.json')
+//   // .then below runs when the remote server responds
+//   .then(function(response) {
+//     // response.text() returns a new promise that resolves with the full response text
+//     // when it loads
+//     return response.text();
+//   })
+//   .then(function(text) {
+//     // ...and here's the content of the remote file
+//     alert(text); // {"name": "iliakan", "isAdmin": true}
+//   })
+
+// we can make  request to GitHub, load the user profile and show the avatar:
+
+// fetch('/article/promise-chaining/user.json')
+//   .then(response => response.json())
+//   .then(user => fetch(`https://api.github.com/users/${user.name}`))
+//   .then(response => response.json())
+//   .then(githubUser => new Promise(function(resolve, reject) { // (*)
+//     let img = document.createElement('img');
+//     img.src = githubUser.avatar_url;
+//     img.className = "promise-avatar-example";
+//     document.body.append(img);
+
+//     setTimeout(() => {
+//       img.remove();
+//       resolve(githubUser); // (**)
+//     }, 3000);
+//   }))
+//   // triggers after 3 seconds
+//   .then(githubUser => alert(`Finished showing ${githubUser.name}`));
+
+// split code into reusable functions
+
+function loadJson(url) {
+  return fetch(url)
+    .then(response => response.json());
 }
 
-new Promise(resolve => resolve(1))
-  .then(result => {
-    return new Thenable(result); // (*)
-  })
-  .then(alert); // shows 2 after 1000ms
+function loadGithubUser(name) {
+  return loadJson(`https://api.github.com/users/${name}`);
+}
+
+function showAvatar(githubUser) {
+  return new Promise(function(resolve, reject) {
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = "promise-avatar-example";
+    document.body.append(img);
+
+    setTimeout(() => {
+      img.remove();
+      resolve(githubUser);
+    }, 3000);
+  });
+}
+
+// Use them:
+loadJson('/article/promise-chaining/user.json')
+  .then(user => loadGithubUser(user.name))
+  .then(showAvatar)
+  .then(githubUser => alert(`Finished showing ${githubUser.name}`));
+  // ...
