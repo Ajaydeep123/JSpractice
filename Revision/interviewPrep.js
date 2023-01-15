58699bee2dafc71a8e2f59897d6c5a6a9e3302ce
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-01-15 23:52:33 +0530

    📝

--- Revision/interviewPrep.js
+++ Revision/interviewPrep.js
@@ -1,19 +1,32 @@
-function bark() {
-    console.log('Woof!');
-  }
+// function bark() {
+//     console.log('Woof!');
+//   }
   
-  bark.animal = 'dog';  //This is possible in JavaScript, because functions are objects! (Everything besides primitive types are objects)
+//   bark.animal = 'dog';  //This is possible in JavaScript, because functions are objects! (Everything besides primitive types are objects)
 
 //   A function is a special type of object. The code you write yourself isn't the actual function. The function is an object with properties. This property is invocable.
+
+// function Person(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+  
+//   const member = new Person('Lydia', 'Hallie');
+//   Person.getFullName = function() {
+//     return `${this.firstName} ${this.lastName}`;
+//   };
+  
+//   console.log(member.getFullName()); //TypeError
+
 
 function Person(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
   }
   
-  const member = new Person('Lydia', 'Hallie');
-  Person.getFullName = function() {
-    return `${this.firstName} ${this.lastName}`;
-  };
+  const lydia = new Person('Lydia', 'Hallie');
+  const sarah = Person('Sarah', 'Smith');
   
-  console.log(member.getFullName()); //TypeError
+  console.log(lydia); // Person {firstName: "Lydia", lastName: "Hallie"}
+
+  console.log(sarah); // undefined
