bb76ce33410eb41daad49398775259ac55c8655f
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-01-19 19:04:45 +0530

    3

--- DSAwithJS/Loop.js
+++ DSAwithJS/Loop.js
@@ -30,13 +30,27 @@
 // larger(10, 20)
 
 // Write a JavaScript conditional statement to find the sign of product of three numbers. Display an alert box with the specified sign.
-let x = parseInt(prompt('enter 1st number')),
-y = parseInt(prompt('enter the 2nd number')),
-z = parseInt(prompt('enter the third number'));
-const result = x*y*z;
-if (result>0) {
-alert('it is +ve');
-}
-else if (result ==0) { alert ('it is zero ')}
+// let x = parseInt(prompt('enter 1st number')),
+// y = parseInt(prompt('enter the 2nd number')),
+// z = parseInt(prompt('enter the third number'));
+// const result = x*y*z;
+// if (result>0) {
+// alert('it is +ve');
+// }
+// else if (result ==0) { alert ('it is zero ')}
 
-else { alert ('the value is -ve')}
+// else { alert ('the value is -ve')}
+
+// Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+
+// for (var x=0; x<=15; x++) {
+//     if (x === 0) {
+//             console.log(x +  " is even");
+//     }
+//     else if (x % 2 === 0) {
+//             console.log(x + " is even");   
+//     }
+//     else {
+//             console.log(x + " is odd");
+//     }
+// }
