655390af9ff7ffd3baf3a6cd8467fd599df12cfe
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-01-20 11:59:36 +0530

    5

--- DSAwithJS/JavaScriptQuestions.js
+++ DSAwithJS/JavaScriptQuestions.js
@@ -84,3 +84,38 @@
 //         } else if (avg < 100) {
 //                 console.log("Grade : A"); 
 // }
+
+// Write a JavaScript function that reverse a number.
+
+// const num = prompt('enter the number',0);
+// const x = (numArg) => {
+// return +[...String(numArg)].reverse().join("");
+// };
+// console.log(x(num));
+
+
+/* 
+Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+Add the number to the end of the array, then remove the first element of the array.
+The nextInLine function should then return the element that was removed. */
+
+/* 
+function nextInLine(arr, item) {
+    // Only change code below this line
+    //push at last
+    arr.push(item);
+    //remove first element of array
+    return arr.shift()
+    
+    return item;
+    // Only change code above this line
+  }
+  
+  // Setup
+  let testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
+ */
