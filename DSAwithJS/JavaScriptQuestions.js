23c40020aa4abc76549cda410b70b23c5fe8b7ec
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-01-22 21:59:13 +0530

    📝

--- DSAwithJS/JavaScriptQuestions.js
+++ DSAwithJS/JavaScriptQuestions.js
@@ -173,4 +173,14 @@ if(arr.indexOf(elem)>=0){
   // Only change code above this line
 }
 
-console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms')); */
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms')); */ 
+/* 
+celsius to fahrenheit
+
+function convertCtoF(celsius) {
+  let fahrenheit = celsius * (9 / 5) + 32;
+  return fahrenheit;
+}
+
+// Change the inputs below to test your code
+convertCtoF(30); */
